6b3e550275126b00cc258fe0f96fc94e19dc5547
sbjang <sbjang123456@gmail.com>
2022-05-31 23:39:15 +0900

    initial: Initial Structure Commit

---
+++ packages/eslint-config-bases/src/bases/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  jest: require('./jest'),
+  react: require('./react'),
+  regexp: require('./regexp'),
+  reactTestingLibrary: require('./rtl'),
+  typescript: require('./typescript'),
+};
